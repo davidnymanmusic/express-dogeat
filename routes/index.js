e6b698fab837d08bd84e5ca73fea2c8c942ca76e
David Nyman <davenyman@gmail.com>
2018-01-15 22:28:45 -0500

    add js for stylesheet changes

--- routes/index.js
+++ routes/index.js
@@ -12,27 +12,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// router.get('/:food', (req, res) => {
-//   const food_name = req.params.food;
-//   knex('food_table').then((rows) => {
-//     function search(nameKey, rowsArray){
-//     for (var i=0; i < rowsArray.length; i++) {
-//         if (rowsArray[i].name === nameKey) {
-//             return rowsArray[i];
-//         }
-//     }
-// }
-//     res.format({
-//       'application/json': () => res.json(rows),
-//       'text/html': () => res.render('home', {
-//         param: food_name,
-//         name: search(food_name, rows).name,
-//         species: search(food_name, rows).description,
-//         title: 'my family' }),
-//       'default': () => res.sendStatus(406)
-//     });
-//   });
-// });
+
 
 router.get('/food', (req, res) => {
   knex('food_table').then((rows) => {
@@ -57,7 +37,7 @@ router.get('/:food_name', (req, res) => {
     res.format({
       'application/json': () => res.json(rows),
       'text/html': () => res.render('home', {
-        title: (food_name + "!").toUpperCase(),
+        title: (search(food_name, rows).edible + "!").toUpperCase(),
         name: search(food_name, rows).food_name,
         description: search(food_name, rows).description,
         }),
